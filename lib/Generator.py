120ec940819d2e5fb9e5dbd19d3ad5357984572c
Robert Strickland <strickland.rbls@gmail.com>
2022-02-05 01:36:05 -0600

    Didn't change much but got different numbers?

--- lib/Generator.py
+++ lib/Generator.py
@@ -18,15 +18,20 @@ class Generator():
         self.total = total
         self.rarity_limits = {}
 
-    def populate_layers(self):
-        return System.get_toplevel_layers()
-
     def parse_permutations(self):
         for nft_id in self.permutations:
             layers = []
             for i in range(len(self.indexes)):
-                layers.append(self.layers[self.categories[i]][self.permutations[nft_id][i]])
+                category = self.__get_layers_category_from_indexes(i)
+                permutation = self.__get_layers_from_permutation_set(nft_id, i)
+                layers.append(self.layers[category][permutation])
             self.nfts.append(NFT(nft_id, layers, self.categories, self.generation_order))
+
+    def __get_layers_category_from_indexes(self, i):
+        return self.categories[i]
+
+    def __get_layers_from_permutation_set(self, nft_id, i):
+        return self.permutations[nft_id][i]
 
     def run(self):
         for i in range(len(self.categories)):
--- main.py
+++ main.py
@@ -3,10 +3,10 @@ from lib.Generator import Generator
 G = Generator()
 
 G.parse_permutations()
-G.run()
+# G.run()
 
 for nft in G.nfts:
-    nft.populate_imgs()
-    nft.generate_image()
+    # nft.populate_imgs()
+    # nft.generate_image()
     nft.generate_metadata()
 
