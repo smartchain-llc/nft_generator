65faa66fed3511669126ce62b2e30f823c9ffc9b
Robert Strickland <strickland.rbls@gmail.com>
2022-02-04 23:29:34 -0600

    back to normal

--- main.py
+++ main.py
@@ -4,10 +4,9 @@ G = Generator()
 
 G.parse_permutations()
 G.run()
-nfts_by_rarity = G.get_nfts_by_rarity()
 
 for nft in G.nfts:
-    # nft.populate_imgs()
-    # nft.generate_image()
+    nft.populate_imgs()
+    nft.generate_image()
     nft.generate_metadata()
 
